dbb9892e3159d90f49303747be7e5c8ee938bfa8
Shuangshuang Zhao <poppindouble@gmail.com>
2017-12-21 22:58:20 -0800

    cleaner

--- relative_ranks.py
+++ relative_ranks.py
@@ -1,14 +1,18 @@
 class Solution:
-	def findRelativeRanks(self, nums):
-		newNums = sorted(nums, reverse=True)
-		medals = ["Gold Medal", "Silver Medal", "Bronze Medal"]
-		myDict = {}
-		for index, num in enumerate(newNums):
-			if index < 3:
-				myDict[num] = medals[index]
-				continue
-			myDict[num] = str(index + 1)
-		return list(map(lambda x: myDict[x], nums))
+	# def findRelativeRanks(self, nums):
+		# newNums = sorted(nums, reverse=True)
+		# medals = ["Gold Medal", "Silver Medal", "Bronze Medal"]
+		# myDict = {}
+		# for index, num in enumerate(newNums):
+		# 	if index < 3:
+		# 		myDict[num] = medals[index]
+		# 		continue
+		# 	myDict[num] = str(index + 1)
+		# return list(map(lambda x: myDict[x], nums))
+
+		def findRelativeRanks(self, nums):
+			dmap = {v : k for k, v in enumerate(sorted(nums, reverse=True))}
+			return [str(dmap[n] + 1) if dmap[n] > 2 else ['Gold', 'Silver', 'Bronze'][dmap[n]] + ' Medal' for n in nums]
 
 def main():
 	print(Solution().findRelativeRanks([5, 8, 1, 3, 9]))
